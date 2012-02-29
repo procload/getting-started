6fed5839ec9c867d4e34d37e6ebf98e8cb597674
Bobby Uhlenbrock <bobby@helloample.com>
2012-02-29 14:01:23 -0500

    Sample lorem usage

--- config.rb
+++ config.rb
@@ -1,61 +1,9 @@
-### 
-# Compass
-###
-
-# Susy grids in Compass
-# require 'susy'
-
-# 960.gs grids in Compass
-# First: gem install compass-960-plugin
-# require 'ninesixty'
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Haml
-###
-
-# CodeRay syntax highlighting in Haml
-# First: gem install haml-coderay
-# require 'haml-coderay'
-
-# CoffeeScript filters in Haml
-# First: gem install coffee-filter
-# require 'coffee-filter'
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-###
-# Page command
-###
-
-# Per-page layout changes:
-# 
-# With no layout
-# page "/path/to/file.html", :layout => false
-# 
-# With alternative layout
-# 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-# require "kss"
-# page "/styleguide/*", :layout => :styleguide do
-#   @styleguide = Kss::Parser.new('source/css')
-# end
-
-###
-# Helpers
-###
+require 'compass'
+require 'bootstrap-sass'
 
 # Methods defined in the helpers block are available in templates
 helpers do
+  
   # Calculate the years for a copyright
   def copyright_years(start_year)
     end_year = Date.today.year
@@ -66,38 +14,4 @@ helpers do
     end
   end
   
-end
-
-
-
-# Change the CSS directory
-# set :css_dir, "alternative_css_directory"
-
-# Change the JS directory
-# set :js_dir, "alternative_js_directory"
-
-# Change the images directory
-# set :images_dir, "alternative_image_directory"
-
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-  
-  # Minify Javascript on build
-  # activate :minify_javascript
-  
-  # Enable cache buster
-  # activate :cache_buster
-  
-  # Use relative URLs
-  # activate :relative_assets
-  
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-  
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
 end
